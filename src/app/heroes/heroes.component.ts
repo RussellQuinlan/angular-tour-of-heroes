75a67629daae2357c3b9616e40fbabe02b25cec7
Quinlan <rquinlan1@iconectiv.com>
2019-05-14 12:45:12 -0400

    Display hero info

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 
 import { HeroService } from '../hero.service';
 
@@ -12,11 +11,7 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero[];
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }  
+  heroes: Hero[]; 
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
